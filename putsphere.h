adacafc29c20720251d5c5fc7dd38e1be89aad2b
marianateixeira065 <mariana.teixeira.065@ufrn.edu.br>
2026-06-10 14:35:36 -0300

    Update putsphere.h

--- putsphere.h
+++ putsphere.h
@@ -3,13 +3,40 @@
 
 #include "figurageometrica.h"
 
+/**
+ * @brief Classe concreta para desenhar uma esfera maciça.
+ */
 class PutSphere : public FiguraGeometrica {
 private:
-    int xcenter, ycenter, zcenter, radius;
+    int xcenter; /**< @brief Coordenada X do centro da esfera */
+    int ycenter; /**< @brief Coordenada Y do centro da esfera */
+    int zcenter; /**< @brief Coordenada Z do centro da esfera */
+    int radius;  /**< @brief Raio da esfera */
+
 public:
+    /**
+     * @brief Construtor da classe PutSphere.
+     * * @param xcenter Coordenada X do centro.
+     * @param ycenter Coordenada Y do centro.
+     * @param zcenter Coordenada Z do centro.
+     * @param radius Raio da esfera.
+     * @param r Cor vermelha.
+     * @param g Cor verde.
+     * @param b Cor azul.
+     * @param a Transparência.
+     */
     PutSphere(int xcenter, int ycenter, int zcenter, int radius, float r, float g, float b, float a);
+
+    /**
+     * @brief Destrutor da classe PutSphere.
+     */
     ~PutSphere() {}
+
+    /**
+     * @brief Desenha a esfera na matriz tridimensional.
+     * * @param t Referência para o objeto Sculptor.
+     */
     void draw(Sculptor &t);
 };
 
-#endif
+#endif // PUTSPHERE_H
