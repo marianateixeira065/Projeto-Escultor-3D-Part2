556e5fd4d49a5b4a84c3162a614dcbe55ec48e30
marianateixeira065 <mariana.teixeira.065@ufrn.edu.br>
2026-06-10 14:34:17 -0300

    Update cutvoxel.h

--- cutvoxel.h
+++ cutvoxel.h
@@ -3,13 +3,34 @@
 
 #include "figurageometrica.h"
 
+/**
+ * @brief Classe concreta para apagar (desativar) um único voxel.
+ */
 class CutVoxel : public FiguraGeometrica {
 private:
-    int x, y, z;
+    int x; /**< @brief Coordenada X do voxel a ser apagado */
+    int y; /**< @brief Coordenada Y do voxel a ser apagado */
+    int z; /**< @brief Coordenada Z do voxel a ser apagado */
+
 public:
+    /**
+     * @brief Construtor da classe CutVoxel.
+     * * @param x Coordenada X do bloco.
+     * @param y Coordenada Y do bloco.
+     * @param z Coordenada Z do bloco.
+     */
     CutVoxel(int x, int y, int z);
-    ~CutVoxel() {};
+
+    /**
+     * @brief Destrutor da classe CutVoxel.
+     */
+    ~CutVoxel() {}
+
+    /**
+     * @brief Apaga o voxel na matriz tridimensional (modifica show para false).
+     * * @param t Referência para o objeto Sculptor.
+     */
     void draw(Sculptor &t);
 };
 
-#endif
+#endif // CUTVOXEL_H
