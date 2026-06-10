8b0f1c818c3389f21b817227d337f0ee5eaccfcc
marianateixeira065 <mariana.teixeira.065@ufrn.edu.br>
2026-06-10 14:36:03 -0300

    Update cutsphere.h

--- cutsphere.h
+++ cutsphere.h
@@ -3,13 +3,36 @@
 
 #include "figurageometrica.h"
 
+/**
+ * @brief Classe concreta para apagar uma esfera maciça.
+ */
 class CutSphere : public FiguraGeometrica {
 private:
-    int xcenter, ycenter, zcenter, radius;
+    int xcenter; /**< @brief Coordenada X do centro da esfera */
+    int ycenter; /**< @brief Coordenada Y do centro da esfera */
+    int zcenter; /**< @brief Coordenada Z do centro da esfera */
+    int radius;  /**< @brief Raio da esfera a ser apagada */
+
 public:
+    /**
+     * @brief Construtor da classe CutSphere.
+     * * @param xcenter Coordenada X do centro.
+     * @param ycenter Coordenada Y do centro.
+     * @param zcenter Coordenada Z do centro.
+     * @param radius Raio da esfera.
+     */
     CutSphere(int xcenter, int ycenter, int zcenter, int radius);
+
+    /**
+     * @brief Destrutor da classe CutSphere.
+     */
     ~CutSphere() {}
+
+    /**
+     * @brief Apaga a esfera na matriz tridimensional.
+     * * @param t Referência para o objeto Sculptor.
+     */
     void draw(Sculptor &t);
 };
 
-#endif
+#endif // CUTSPHERE_H
