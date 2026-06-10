14c7f53c34c93803f3c7fca9ceaf661231872e87
marianateixeira065 <mariana.teixeira.065@ufrn.edu.br>
2026-06-10 14:36:29 -0300

    Update putellipsoid.h

--- putellipsoid.h
+++ putellipsoid.h
@@ -3,13 +3,44 @@
 
 #include "figurageometrica.h"
 
+/**
+ * @brief Classe concreta para desenhar um elipsoide maciço.
+ */
 class PutEllipsoid : public FiguraGeometrica {
 private:
-    int xcenter, ycenter, zcenter, rx, ry, rz;
+    int xcenter; /**< @brief Coordenada X do centro do elipsoide */
+    int ycenter; /**< @brief Coordenada Y do centro do elipsoide */
+    int zcenter; /**< @brief Coordenada Z do centro do elipsoide */
+    int rx;      /**< @brief Raio do elipsoide no eixo X */
+    int ry;      /**< @brief Raio do elipsoide no eixo Y */
+    int rz;      /**< @brief Raio do elipsoide no eixo Z */
+
 public:
+    /**
+     * @brief Construtor da classe PutEllipsoid.
+     * * @param xcenter Coordenada X do centro.
+     * @param ycenter Coordenada Y do centro.
+     * @param zcenter Coordenada Z do centro.
+     * @param rx Raio no eixo X.
+     * @param ry Raio no eixo Y.
+     * @param rz Raio no eixo Z.
+     * @param r Cor vermelha.
+     * @param g Cor verde.
+     * @param b Cor azul.
+     * @param a Transparência.
+     */
     PutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz, float r, float g, float b, float a);
+
+    /**
+     * @brief Destrutor da classe PutEllipsoid.
+     */
     ~PutEllipsoid() {}
+
+    /**
+     * @brief Desenha o elipsoide na matriz tridimensional.
+     * * @param t Referência para o objeto Sculptor.
+     */
     void draw(Sculptor &t);
 };
 
-#endif
+#endif // PUTELLIPSOID_H
